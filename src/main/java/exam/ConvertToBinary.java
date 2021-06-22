56f41c624743883ddf6c2db71a56adaac509e6f3
utkarshagove <56468200+utkarshagove@users.noreply.github.com>
2021-06-22 12:31:39 +0530

    Sample.Java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,32 @@
-package exam;
-
-public class ConvertToBinary {
-
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+import java.io.*;
+  
+class Sample 
+{
+    // function to convert decimal to binary
+    static void decToBinary(int n)
+    {
+        // array to store binary number
+        int[] binaryNum = new int[1000];
+   
+        // counter for binary array
+        int i = 0;
+        while (n > 0) 
+        {
+            // storing remainder in binary array
+            binaryNum[i] = n % 2;
+            n = n / 2;
+            i++;
+        }
+   
+        // printing binary array in reverse order
+        for (int j = i - 1; j >= 0; j--)
+            System.out.print(binaryNum[j]);
+    }
+      
+    // driver program
+    public static void main (String[] args) 
+    {
+        int n = 35;
+        decToBinary(n);
+    }
 }
