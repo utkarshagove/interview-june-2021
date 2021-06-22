60cf7c62e3df2a599148cac794af32ab755c4e69
utkarshagove <56468200+utkarshagove@users.noreply.github.com>
2021-06-22 13:30:14 +0530

    CountCombination.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -2,9 +2,41 @@ package exam;
 
 public class CountCombinations {
 
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
-
+  static void combinationUtil(int arr[], int data[], int start,
+                                int end, int index, int r)
+    {
+        
+        if (index == r)
+        {
+            for (int j=0; j<r; j++)
+                System.out.print(data[j]+" ");
+            System.out.println("");
+            return;
+        }
+ 
+        
+        for (int i=start; i<=end && end-i+1 >= r-index; i++)
+        {
+            data[index] = arr[i];
+            combinationUtil(arr, data, i+1, end, index+1, r);
+        }
+    }
+ 
+    
+    static void printCombination(int arr[], int n, int r)
+    {
+        
+        int data[]=new int[r];
+ 
+       
+        combinationUtil(arr, data, 0, n-1, 0, r);
+    }
+ 
+    /*Driver function to check for above function*/
+    public static void main (String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        int r = 3;
+        int n = arr.length;
+        printCombination(arr, n, r);
+    }
 }
